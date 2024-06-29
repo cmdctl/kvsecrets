78063c47d52e2f5c2663b549b85c99e8460b2c19
cmdctl <anton.bozhinov@gmail.com>
2024-06-30 01:31:36 +0300

    finish

---
+++ go.mod
@@ -0,0 +1,5 @@
+module secrets
+
+go 1.22.1
+
+require github.com/sergi/go-diff v1.3.1 // indirect
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+)
+
+type KeyVaultResponse []struct {
+	Attributes struct {
+		Created         time.Time   `json:"created"`
+		Enabled         bool        `json:"enabled"`
+		Expires         interface{} `json:"expires"`
+		NotBefore       interface{} `json:"notBefore"`
+		RecoverableDays int         `json:"recoverableDays"`
+		RecoveryLevel   string      `json:"recoveryLevel"`
+		Updated         time.Time   `json:"updated"`
+	} `json:"attributes"`
+	ContentType interface{} `json:"contentType"`
+	ID          string      `json:"id"`
+	Managed     interface{} `json:"managed"`
+	Name        string      `json:"name"`
+	Tags        struct {
+	} `json:"tags"`
+}
+
+func getValuesFromKeys(name string, keys []string) map[string]string {
+	query := `az keyvault secret show --name %s --vault-name %s --query value -o tsv`
+	values := make(map[string]string)
+	var wg sync.WaitGroup
+	mu := &sync.Mutex{}
+
+	for _, key := range keys {
+		wg.Add(1)
+		go func(k string) {
+			defer wg.Done()
+			cmd := exec.Command("bash", "-c", fmt.Sprintf(query, k, name))
+			out, err := cmd.Output()
+			if err != nil {
+				fmt.Printf("Error fetching key %s: %v\n", k, err)
+				return
+			}
+			mu.Lock()
+			values[k] = string(out)
+			mu.Unlock()
+		}(key)
+	}
+
+	wg.Wait()
+	return values
+}
+
+func ListKvKeys(name string) []string {
+	var response KeyVaultResponse
+	const query = `az keyvault secret list --vault-name %s`
+	cmd := exec.Command("bash", "-c", fmt.Sprintf(query, name))
+	out, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(out, &response)
+	if err != nil {
+		panic(err)
+	}
+	var keys []string
+	for _, key := range response {
+		keys = append(keys, key.Name)
+	}
+	return keys
+}
+
+func displayUsage() {
+	fmt.Println("Usage: secrets show <vault-name>")
+	fmt.Println("Usage: secrets diff <vault-name> <vault-name>")
+}
+
+func getKvKeyValPairsAsString(name string) string {
+	keys := ListKvKeys(name)
+	values := getValuesFromKeys(name, keys)
+
+	// Extract the keys and sort them
+	var sortedKeys []string
+	for key := range values {
+		sortedKeys = append(sortedKeys, key)
+	}
+	sort.Strings(sortedKeys)
+
+	// Iterate over the sorted keys to access the values
+	var kvPairs string
+	for _, key := range sortedKeys {
+		kvPairs += fmt.Sprintf("%s: %s", key, values[key])
+	}
+
+	return kvPairs
+}
+
+
+func main() {
+	args := os.Args
+	if len(args) < 3 {
+		displayUsage()
+		return
+	}
+
+	switch args[1] {
+	case "show":
+		keys := ListKvKeys(args[2])
+		fmt.Printf("Keys in %s:\n", args[2])
+		fmt.Printf("Found %d keys\n", len(keys))
+		values := getValuesFromKeys(args[2], keys)
+		for key, value := range values {
+			fmt.Printf("%s: %s", key, value)
+		}
+
+	case "diff":
+		fmt.Println("Comparing keys in", args[2], "and", args[3])
+		keys1 := getKvKeyValPairsAsString(args[2])
+		keys2 := getKvKeyValPairsAsString(args[3])
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(keys1, keys2, false)
+		fmt.Println(dmp.DiffPrettyText(diffs))
+
+	default:
+		fmt.Println("Invalid command")
+	}
+}
+
